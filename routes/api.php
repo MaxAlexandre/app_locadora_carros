687723a83467f0c4f232f99e1857ca3c48417b5d
MaxFerreira <max.alexxandre@gmail.com>
2024-06-05 17:42:37 -0300

    Implementando os métodos de autenticação  token JWT

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 //Route::resource('cliente', 'App\Http\Controllers\ClienteController');
 Route::prefix('v1')->middleware('jwt.auth')->group(function () {
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
     Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
     Route::apiResource('carro', 'App\Http\Controllers\CarroController');
     Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
@@ -29,6 +32,3 @@ Route::prefix('v1')->middleware('jwt.auth')->group(function () {
 
 
 Route::post('login', 'App\Http\Controllers\AuthController@login');
-Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-Route::post('me', 'App\Http\Controllers\AuthController@me');
