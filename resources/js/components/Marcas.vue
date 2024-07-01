a8a5535525adc85f030fe9895be8575f4af4e433
MaxFerreira <max.alexxandre@gmail.com>
2024-07-01 19:24:55 -0300

    Criando Modal para inclusão de marcas e implementando o consumo da api para inserção.

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -7,13 +7,17 @@
                     <template v-slot:conteudo>
                         <div class="form-row">
                             <div class="col mb-3">
-                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca.">
-                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                                           texto-ajuda="Opcional. Informe o ID da marca.">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                           placeholder="ID">
                                 </input-container-component>
                             </div>
                             <div class="col mb-3">
-                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca.">
-                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
+                                                           texto-ajuda="Opcional. Informe o nome da marca.">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                                           placeholder="Nome da marca">
                                 </input-container-component>
                             </div>
                         </div>
@@ -32,16 +36,87 @@
                     </template>
 
                     <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal"
+                                data-target="#modalMarca">Adicionar
+                        </button>
                     </template>
 
                 </card-component>
                 <!--Fim do card de listagem de marcas-->
             </div>
         </div>
+        <modal-component id="modalMarca" titulo="Adicionar marca">
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp"
+                                               texto-ajuda="Informe o nome da marca.">
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
+                               placeholder="Nome da marca" v-model="nomeMarca">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem da marca" id="novoImagem" id-help="novoImagemHelp"
+                                               texto-ajuda="Selecione uma imagem (PNG ou JPG)">
+                        <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp"
+                               placeholder="Selecione uma Imagem" @change="carregarImagem($event)">
+                    </input-container-component>
+                </div>
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+            </template>
+        </modal-component>
     </div>
 </template>
 
 <script>
+export default {
+    computed: {
+        token() {
+            let token = document.cookie.split(';').find(indice => {
+                return indice.includes('token=')
+            })
 
+            token = token.split('=')[1]
+            token = 'Bearer ' + token
+
+            return token
+        }
+    },
+    data() {
+        return {
+            urlBase: 'http://localhost:8000/api/v1/marca',
+            nomeMarca: '',
+            arquivoImagem: []
+        }
+    },
+    methods: {
+        carregarImagem(e) {
+            this.arquivoImagem = e.target.files
+        },
+        salvar() {
+            let formData = new FormData();
+            formData.append('nome', this.nomeMarca)
+            formData.append('imagem', this.arquivoImagem[0])
+
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+            axios.post(this.urlBase, formData, config)
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(errors => {
+                    console.log(errors)
+                })
+        }
+    }
+}
 </script>
